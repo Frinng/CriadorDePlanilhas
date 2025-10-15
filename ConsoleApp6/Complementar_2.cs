035d0e90a925dc802a1c885df7eac6823f7400f7
Frinng <167120714+Frinng@users.noreply.github.com>
2025-10-15 13:57:26 -0300

    Mudancas completas

--- ConsoleApp6/Complementar_2.cs
+++ ConsoleApp6/Complementar_2.cs
@@ -1,22 +1,20 @@
-﻿namespace ConsoleApp6;
+﻿﻿namespace ConsoleApp6;
 using ClosedXML.Excel;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Complementar_2 {
+    
+    public static string NumeroItem;
+    public static int N1 = 0;
+    public static int numeroitem = 0;
+    public static int quantidaAtual;
+    public static void LerPlanilhas()
+    {
 
-    public static void AlterarDadosDaplanilha() {
-
-          
-
-    }
-
-    public static void LerPlanilhas() {
+        string CaminhoDoEstoque =  "C:\\Users\\aluno_iot\\Documents\\Pedro\\ProjetoTestePlanilha\\Simulacao Estoque.xlsx";
         
-        if (Complementar.CaminhoCOmpleto != null) {
-         
-            string CaminhoDaLeitura = Complementar.CaminhoCOmpleto;
-
-            using (var LivroDeTrabalho = new XLWorkbook(CaminhoDaLeitura)) {
+        using (var LivroDeTrabalho = new XLWorkbook(CaminhoDoEstoque)) {
 
                 var Planilha = LivroDeTrabalho.Worksheet(1);
                 
@@ -40,7 +38,7 @@ public class Complementar_2 {
                     string marca = linha.Cell(4).GetValue<string>();
                     string local = linha.Cell(5).GetValue<string>();
                     
-                    Console.WriteLine($"|  {item,-4}  | {Descri, 20}  | {marca,12}  | {local}  |");
+                    Console.WriteLine($"|  {item,-4}  | {Descri, 20}  |  {quantidade}  |  {marca,12}  | {local}  |");
 
                 }
                 
@@ -52,12 +50,158 @@ public class Complementar_2 {
 
                 Console.ReadKey();
             }
-            
+        
+    }
+
+    public static void NumeroqueVoceDesejaAlterar() {
+        
+        Console.WriteLine("Digite o numero do item que voce deseja alterar: ");
+        NumeroItem = Console.ReadLine();
+        
+        if (int.TryParse(NumeroItem, out N1)) {
+
+            numeroitem = N1;
+
         }else {
-            
-            FuncoesUteis.VOCENAOCADASTROUNADA();
-            
+                    
+            FuncoesUteis.DIGITEUMNUMEROINTEIRO();
+                    
         }
         
+    }
+    
+    public static void AlterarDadosDaplanilha() {
+        if (Complementar.CaminhoCOmpleto != null) {
+            
+            string CaminhoDaAlteração = Complementar.CaminhoCOmpleto;
+
+            using (var LivroTrabalhokkkOloco = new XLWorkbook(CaminhoDaAlteração)) {
+
+                var Planilha = LivroTrabalhokkkOloco.Worksheet(1);
+
+                int linhaencontrada = -1;
+                string nome;
+                string adicionarstring;
+                int adicionar = 0;
+                string retirarstring;
+                int retirar = 0;
+                string Opcaostring;
+                int Opcao = 0;
+
+                NumeroqueVoceDesejaAlterar();
+
+                foreach (var linha in Planilha.RowsUsed().Skip(1)) {
+                    if (linha.Cell(1).GetValue<int>() == numeroitem) {
+
+                        linhaencontrada = linha.RowNumber();
+                        
+                        break;
+
+                    }
+                }
+
+                if (linhaencontrada == -1) {
+
+                    FuncoesUteis.ITEMNAOENCONTRADO();
+                    
+                    return;
+                }
+
+                var celulaquantida = Planilha.Cell(linhaencontrada, 3);
+                quantidaAtual = celulaquantida.GetValue<int>();
+                nome = Planilha.Cell(linhaencontrada, 2).GetValue<string>();
+                
+                Console.WriteLine($"Quantidade Atual de {nome}: {quantidaAtual}");
+                
+                Console.WriteLine(" ");
+                
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("|                você deseja                 |");
+                Console.WriteLine("----------------------------------------------");
+                
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("| 1-Adicionar                                |");
+                Console.WriteLine("| 2-Retirar                                  |");
+                Console.WriteLine("----------------------------------------------");
+                Opcaostring =Console.ReadLine();
+
+                if (int.TryParse(Opcaostring, out Opcao)) {
+
+                    switch (Opcao) {
+                        
+                        case 1:
+                            Console.Clear();
+                            
+                            Console.WriteLine("Digite quanto voce deseja adicionar: ");
+                            adicionarstring = Console.ReadLine();
+
+                            if (int.TryParse(adicionarstring, out adicionar)) {
+
+                                quantidaAtual += adicionar;
+                                
+                                celulaquantida.Value = quantidaAtual;
+                                    
+                                LivroTrabalhokkkOloco.Save();
+                            }
+                            else {
+                                
+                                FuncoesUteis.DIGITEUMNUMEROINTEIRO();
+                                
+                            }
+                            
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("Digite quanto voce deseja retirar: ");
+                            retirarstring = Console.ReadLine();
+
+                            if (int.TryParse(retirarstring, out retirar)) {
+
+                                if (quantidaAtual >= retirar) {
+                                    
+                                    quantidaAtual -= retirar;
+                                    
+                                    celulaquantida.Value = quantidaAtual;
+                                    
+                                    LivroTrabalhokkkOloco.Save();
+
+                                    Console.WriteLine("\nAlteração salva com sucesso!");
+                                    
+                                }else {
+                                    
+                                     
+                                    Console.WriteLine("------------------------------------------------------------------");
+                                    Console.WriteLine("|    Quantidade já está em zero, não é possível retirar mais     |");
+                                    Console.WriteLine("------------------------------------------------------------------");
+                                        
+                                }
+
+                            }
+                            else {
+                                
+                                FuncoesUteis.DIGITEUMNUMEROINTEIRO();
+                                
+                            }
+                            
+                            break;
+                        default:
+                            
+                            FuncoesUteis.DIGITEUMAOPCAOVALIDA();
+                            
+                            break;
+                        
+                    }
+                    
+                }else {
+                    
+                    FuncoesUteis.DIGITEUMAOPCAOVALIDA();
+                    
+                }
+            }
+        }else {
+              
+              FuncoesUteis.VOCENAOCADASTROUNADA();
+              
+        }
     }
 }
--- ConsoleApp6/FuncoesUteis.cs
+++ ConsoleApp6/FuncoesUteis.cs
@@ -1,125 +1,148 @@
 ﻿namespace ConsoleApp6;
 
-public class FuncoesUteis {
-    
-        public static void CLIQUEAQUIPARACONTINUAR() {
-                    
-                Console.WriteLine(" ");
-                            
-                Console.WriteLine("-----------------------------------------------");
-                Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
-                Console.WriteLine("-----------------------------------------------");
-                        
-                Console.ReadKey();
-                
-        }
+public class FuncoesUteis
+{
 
-        public static void DIGITEUMAOPCAOVALIDA() {
-            
-            Console.Clear();
-                        
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("|          Digite uma opção Valida            |");
-            Console.WriteLine("-----------------------------------------------");
-                    
-            Console.WriteLine(" ");
-                        
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
-            Console.WriteLine("-----------------------------------------------");
-                    
-            Console.ReadKey();
-            
-        }
+    public static void CLIQUEAQUIPARACONTINUAR()
+    {
 
-        public static void SAINDODOSISTEMA() {
-            
-            Console.Clear();
-            
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("|               Saindo do Sistema             |");
-            Console.WriteLine("-----------------------------------------------");
-                    
-            Console.WriteLine(" ");
-                        
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
-            Console.WriteLine("-----------------------------------------------");
-                    
-            Console.ReadKey();
-            
-            
-        }
+        Console.WriteLine(" ");
 
-        public static void VOLTANDOAOMENU() {
-            
-            Console.Clear();
-            
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("|               Voltando ao Menu              |");
-            Console.WriteLine("-----------------------------------------------");
-                    
-            Console.WriteLine(" ");
-                        
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
-            Console.WriteLine("-----------------------------------------------");
-                    
-            Console.ReadKey();
-            
-        }
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
+        Console.WriteLine("-----------------------------------------------");
 
-        public static void DIGITEUMNUMEROINTEIRO() {
-            
-            Console.Clear();
-            
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("|           Digite um número inteiro          |");
-            Console.WriteLine("-----------------------------------------------");
-                    
-            Console.WriteLine(" ");
-                        
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
-            Console.WriteLine("-----------------------------------------------");
+        Console.ReadKey();
 
-            Console.ReadKey();
-            
-        }
-        
-        public static void DIGITEUMADATAVALIDA() {
-            
-            Console.Clear();
-            
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("|           Digite uma data valida            |");
-            Console.WriteLine("-----------------------------------------------");
-                    
-            Console.WriteLine(" ");
-                        
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
-            Console.WriteLine("-----------------------------------------------");
+    }
 
-            Console.ReadKey();
-            
-        }
+    public static void DIGITEUMAOPCAOVALIDA()
+    {
 
-        public static void VOCENAOCADASTROUNADA() {
-            
-            Console.Clear();
-            
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("|          Você não cadastrou nada            |");
-            Console.WriteLine("-----------------------------------------------");
-            
-            Console.WriteLine(" ");
-                        
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
-            Console.WriteLine("-----------------------------------------------");
+        Console.Clear();
 
-            Console.ReadKey();
-        }
-    
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|          Digite uma opção Valida            |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.ReadKey();
+
+    }
+
+    public static void SAINDODOSISTEMA()
+    {
+
+        Console.Clear();
+
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|               Saindo do Sistema             |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.ReadKey();
+
+
+    }
+
+    public static void VOLTANDOAOMENU()
+    {
+
+        Console.Clear();
+
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|               Voltando ao Menu              |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.ReadKey();
+
+    }
+
+    public static void DIGITEUMNUMEROINTEIRO()
+    {
+
+        Console.Clear();
+
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|           Digite um número inteiro          |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.ReadKey();
+
+    }
+
+    public static void DIGITEUMADATAVALIDA()
+    {
+
+        Console.Clear();
+
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|           Digite uma data valida            |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.ReadKey();
+
+    }
+
+    public static void VOCENAOCADASTROUNADA()
+    {
+
+        Console.Clear();
+
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|          Você não cadastrou nada            |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.ReadKey();
+    }
+
+    public static void ITEMNAOENCONTRADO()
+    {
+
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|            Item não encontrado              |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("|   Pressione qualquer tecla para continuar   |");
+        Console.WriteLine("-----------------------------------------------");
+
+        Console.ReadKey();
+    }
 }
