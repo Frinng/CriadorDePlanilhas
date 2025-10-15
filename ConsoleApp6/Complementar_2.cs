aa0918cb082cefddcc26f5182161429ac160b703
Frinng <167120714+Frinng@users.noreply.github.com>
2025-10-14 21:34:23 -0300

    pix caiu rapaziada

--- ConsoleApp6/Complementar_2.cs
+++ ConsoleApp6/Complementar_2.cs
@@ -9,6 +9,36 @@ public class Complementar_2 {
         if (Complementar.CaminhoCOmpleto != null) {
          
             string CaminhoDaLeitura = Complementar.CaminhoCOmpleto;
+
+            using (var LivroDeTrabalho = new XLWorkbook(CaminhoDaLeitura)) {
+
+                var Planilha = LivroDeTrabalho.Worksheet(1);
+                
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine("|                Estoque Atual                |");
+                Console.WriteLine("-----------------------------------------------");
+                
+                Console.WriteLine(" ");
+                
+                Console.WriteLine("-------------------------------------------------------------------------------");
+                Console.WriteLine("|  Item  |   Descrição   |   Quantidade   |    Marca/Modelo   |  Localização  |");
+                Console.WriteLine("-------------------------------------------------------------------------------");
+                
+                Console.WriteLine(" ");
+
+                foreach (var linha in Planilha.RowsUsed().Skip(1)) {
+
+                    int item = linha.Cell(1).GetValue<int>();
+                    string Descri = linha.Cell(2).GetValue<string>();
+                    int quantidade = linha.Cell(3).GetValue<int>();
+                    string marca = linha.Cell(4).GetValue<string>();
+                    string local = linha.Cell(5).GetValue<string>();
+                    
+                    Console.WriteLine($"{item,-4} | {Descri, 20} | {marca,12} | {local}");
+
+                }
+                
+            }
             
         }else {
             
